b1e9cd54ccc32061ed490c10244703b341a29977
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-03 00:28:57 +0600

    Add files via upload

---
+++ javaScript/loop/script5.js
@@ -0,0 +1,27 @@
+// **for loop**
+// let i
+// for(i=0; i<=10; i++){
+//     console.log("pranto")
+// }
+
+// let num
+// for(num=0; num<10; num++){
+//     console.log(num)
+// } 
+
+
+// **while loop**
+// let num=0;
+// while(num<10){
+//     console.log(num)
+//     num++;
+// }
+
+
+// **do while**
+// let num=0;
+// do{
+//     console.log(num) 
+//     num++ 
+// }while(num<10)
+//     console.log(num)
