74e6a5ddcc1a458065142f3a690aed1a36812008
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-04 13:30:16 +0600

    Add files via upload

---
+++ javaScript/clone-array/script7.js
@@ -0,0 +1,24 @@
+// **clone array**
+
+// ==>spread operator
+// let array1 = ["pranto", "rahim", "karim"]
+// let array2 = ["jakir", "sattar", "joy"]
+// let array3 = [...array1,...array2]
+// console.log(array3)
+
+// ==>concat()
+// let array1 = ["pranto", "rahim", "karim"]
+// let array2 = ["jakir", "sattar", "joy"]
+// let array3 = [].concat(array1, array2)
+// console.log(array3)
+
+// let array1 = ["pranto", "rahim", "karim"]
+// let array2 = ["jakir", "sattar", "joy"]
+// let array3 = array1.concat(array2)
+// console.log(array3)
+
+// ==>slice
+// let array1 = ["pranto", "rahim", "karim"]
+// let array2 = ["jakir", "sattar", "joy"]
+// let array3 = array2.slice(0)
+// console.log(array3)
