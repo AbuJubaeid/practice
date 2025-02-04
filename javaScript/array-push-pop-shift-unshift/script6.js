71dc8f56dedfb8dd39fd3c14780cd746adf9417e
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-04 13:29:06 +0600

    Add files via upload

---
+++ javaScript/array-push-pop-shift-unshift/script6.js
@@ -0,0 +1,38 @@
+// **array**
+// let array = ["pranto", "rahim", "karim"]
+// console.log(array)
+// console.log(typeof array)
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+
+
+
+// **push operation**
+// let array = ["pranto", "rahim", "karim"]
+// console.log(array)
+// console.log(typeof array)
+// array.push("nur")
+// console.log(array)
+
+// **pop operation**
+// let array = ["pranto", "rahim", "karim"]
+// console.log(array)
+// console.log(typeof array)
+// array.pop(array)
+// console.log(array)
+
+
+// **shift operation**
+// let array = ["pranto", "rahim", "karim"]
+// console.log(array)
+// console.log(typeof array)
+// array.shift()
+// console.log(array)
+
+// **unshift operation**
+// let array = ["pranto", "rahim", "karim"]
+// console.log(array)
+// console.log(typeof array)
+// array.unshift("jakir")
+// console.log(array)
