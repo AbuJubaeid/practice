fd4004901bebdc5466cc5ad9855f800332d23705
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-04 13:38:41 +0600

    Update script3.js

--- javaScript/switch-case/script3.js
+++ javaScript/switch-case/script3.js
@@ -1,15 +1,15 @@
 // **switch case**
-// let number = 6
-// switch(number){
-//     case 10:
-//         console.log("sunday")
-//         break
-//     case 5:
-//         console.log("monday")
-//         break
-//     case 2:
-//         console.log("wednesday")
-//         break
-//     default:
-//         console.log("off day")
-// }
+let number = 6
+switch(number){
+    case 10:
+        console.log("sunday")
+        break
+    case 5:
+        console.log("monday")
+        break
+    case 2:
+        console.log("wednesday")
+        break
+    default:
+        console.log("off day")
+}
