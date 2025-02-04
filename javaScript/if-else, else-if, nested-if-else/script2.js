b197c5289ec37bef7f1d758d1026142fbcaf1c75
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-04 13:35:04 +0600

    Update script2.js

--- javaScript/if-else, else-if, nested-if-else/script2.js
+++ javaScript/if-else, else-if, nested-if-else/script2.js
@@ -1,46 +1,46 @@
 // **if else**
-// let number = 21
-// if(number>20){
-//     console.log("it will rain today")
-// }
-// else{
-//     console.log("it will not rain today")
-// }
+let number = 21
+if(number>20){
+    console.log("it will rain today")
+}
+else{
+    console.log("it will not rain today")
+}
 
 // **nested if else....else if**
-// let number = 20
-// if(number>20){
-//     if(number<30){
-//         console.log("it will rain")
-//     }else{
-//         console.log("good weather")
-//     }
-// }
-// else{
-//     console.log("it will not rain today")
-// }
+let number = 20
+if(number>20){
+    if(number<30){
+        console.log("it will rain")
+    }else{
+        console.log("good weather")
+    }
+}
+else{
+    console.log("it will not rain today")
+}
 
-// let temp = 14
-// if(temp<20){
-//         console.log("it will rain")
-//     }
-//     else if(temp<30)
-//         {
-//         console.log("it will rain not today")
-//         }
-//     else{
-//         console.log("good weather")
-//     }
+let temp = 14
+if(temp<20){
+        console.log("it will rain")
+    }
+    else if(temp<30)
+        {
+        console.log("it will rain not today")
+        }
+    else{
+        console.log("good weather")
+    }
 
 // **use prompt() function**
-// let number = prompt()
-// if(number>20){
-//     if(number<30){
-//         console.log("it will rain")
-//     }else{
-//         console.log("good weather")
-//     }
-// }
-// else{
-//     console.log("it will not rain today")
-// }
+let number = prompt()
+if(number>20){
+    if(number<30){
+        console.log("it will rain")
+    }else{
+        console.log("good weather")
+    }
+}
+else{
+    console.log("it will not rain today")
+}
