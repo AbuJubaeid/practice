95a824299e5944c0fc26f9894e430a83efad4b69
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-04 13:39:45 +0600

    Update script4.js

--- javaScript/ternary-operator/script4.js
+++ javaScript/ternary-operator/script4.js
@@ -1,9 +1,9 @@
 // **ternary operator**
-// let number = 25
-// // if(number>20){
-// //     console.log("it will rain today")
-// // }
-// // else{
-// //     console.log("it will not rain today")
-// // }
-// console.log(number>20 ? "it will rain today" : "it will not rain today")
+let number = 25
+if(number>20){
+    console.log("it will rain today")
+}
+else{
+    console.log("it will not rain today")
+}
+console.log(number>20 ? "it will rain today" : "it will not rain today")
