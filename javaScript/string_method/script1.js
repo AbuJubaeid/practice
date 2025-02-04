20e903aa2b1f9f3d732571615494ad5c9521fd25
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-04 13:38:08 +0600

    Update script1.js

--- javaScript/string_method/script1.js
+++ javaScript/string_method/script1.js
@@ -1,47 +1,47 @@
 // **STRING METHOD**
-// let num1 = "pranto"
-// let num = num1.slice(0, 4)
-// console.log(num)
+let num1 = "pranto"
+let num = num1.slice(0, 4)
+console.log(num)
 
 // **string concate**
-// let name1 = "rahim"
-// let name2 = "karim"
-// let name = name1 + name2
-// let names = `my name is ${name1} and his name is ${name2}`
-// console.log(name)
-// console.log(names)
+let name1 = "rahim"
+let name2 = "karim"
+let name = name1 + name2
+let names = `my name is ${name1} and his name is ${name2}`
+console.log(name)
+console.log(names)
 
 // **typeof**
-// let name = "rahim"
-// var array = ["karim"]
-// let person = {
-//     name:"pranto",
-//     age:24,
-// }
-// console.log(typeof name)
-// console.log(typeof array)
-// console.log(typeof person)
+let name = "rahim"
+var array = ["karim"]
+let person = {
+    name:"pranto",
+    age:24,
+}
+console.log(typeof name)
+console.log(typeof array)
+console.log(typeof person)
 
 // **data conversion**
-// let num = 20
-// console.log(typeof num)
-// num = ""+20
-// console.log(typeof num)
+let num = 20
+console.log(typeof num)
+num = ""+20
+console.log(typeof num)
 
-// let num1 = 20
-// num1="num1"
-// console.log(typeof num1)
+let num1 = 20
+num1="num1"
+console.log(typeof num1)
 
-// let num2 = 25
-// num2="num2"+
-// console.log(typeof num2)
+let num2 = 25
+num2="num2"+
+console.log(typeof num2)
 
 
-// let num3 = "25"
-// console.log(typeof num3)
-// num3=Number(num3)
-// console.log(typeof num3)
+let num3 = "25"
+console.log(typeof num3)
+num3=Number(num3)
+console.log(typeof num3)
 
-// let num4 = 20
-// num4=String(num4)
-// console.log(typeof num4)
+let num4 = 20
+num4=String(num4)
+console.log(typeof num4)
