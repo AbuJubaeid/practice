7746b5760ce649662ca2ff8dd534facc176ac09e
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-05 12:43:20 +0600

    Add files via upload

---
+++ javaScript/loop-in-array/script8.js
@@ -0,0 +1,23 @@
+// **for loop**
+// let array = ["pranto", "rahim", "karim"]
+
+// for(i=0; i<array.length; i++)
+// console.log(array[i])
+
+
+// **while loop**
+// let array = ["pranto", "rahim", "karim"]
+// let i=0;
+// while(i<array.length){
+//   console.log(array[i])
+//    i++ 
+// }
+
+// **do while loop**
+// let array = ["pranto", "rahim", "karim"]
+// let i=0;
+// do{
+//    console.log(array[i])
+//    i++  
+// }
+// while(i<array.length)
