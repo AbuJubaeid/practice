54958d66c48518af8e232397c7981fb4538689d3
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-05 12:45:01 +0600

    Add files via upload

---
+++ javaScript/for-of-for-in-loop/script9.js
@@ -0,0 +1,44 @@
+// **for of loop**
+
+// let array = ["pranto", "rahim", "karim"]
+
+// for(i of array){
+//     console.log(i)
+// }
+
+// let array = ["pranto", "rahim", "karim"]
+// let array2 = []
+
+// for(i of array){
+//     array2.push(i) 
+// }
+// console.log(array2)
+
+
+// **for in loop**
+
+// let array = ["pranto", "rahim", "karim"]
+
+// for(i in array){
+//     console.log(i)
+// }
+
+// let array = ["pranto", "rahim", "karim"]
+
+// for(i in array){
+//     console.log(array[i])
+// }
+
+// let array = ["pranto", "rahim", "karim"]
+// let array2 = []
+
+// for(i in array){
+//     array2.push(array[i])
+// }
+// console.log(array2)
+
+// let array = ["pranto", "rahim", "karim"]
+
+// for(i in array){
+//     console.log(array[i].toUpperCase())
+// }
