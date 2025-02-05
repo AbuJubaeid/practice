f2bbafbfc7cfd1577353e90c09da923453f2ebe0
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-05 12:46:37 +0600

    Add files via upload

---
+++ javaScript/array-destructuring/script10.js
@@ -0,0 +1,30 @@
+// **array destructuring**
+
+// let array = ["pranto", "rahim", "karim"]
+
+// let [myArray1, myArray2] = array
+// console.log(myArray1)
+// console.log(myArray2)
+
+
+// let array = ["pranto", "rahim", "karim"]
+
+// let [myArray1, , myArray2] = array
+// console.log(myArray1)
+// console.log(myArray2)
+
+
+// let array = ["pranto", "rahim", "karim", "shanto", "joy"]
+
+// let [myArray1, , myArray2,...myArray3] = array
+// console.log(myArray1)
+// console.log(myArray2)
+// console.log(myArray3)
+
+// let array = ["pranto", "rahim", "karim", "shanto", "joy"]
+
+// let [myArray1, myArray2] = array
+// let myArray3 = array.slice(2)
+// console.log(myArray1)
+// console.log(myArray2)
+// console.log(myArray3)
