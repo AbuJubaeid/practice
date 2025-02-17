9d98f17c09fa2bbe28629c83688bd12defeac50c
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-17 09:26:45 +0600

    Add files via upload

---
+++ javaScript/computed-properties(object)/script13.js
@@ -0,0 +1,45 @@
+// **computed properties**
+
+// let key1 = "objkey1"
+// let key2 = "objkey2"
+
+// let value1 = "myValue1"
+// let value2 = "myValue2"
+
+// let object = {}
+
+// object[key1] = `${value1}`
+// object[key2] = `${value2}`
+
+// console.log(object)
+
+// ==> another way
+
+// let key1 = "objkey1"
+// let key2 = "objkey2"
+
+// let value1 = "myValue1"
+// let value2 = "myValue2"
+
+// let object = {}
+
+// object[key1] = value1
+// object[key2] = value2
+
+// console.log(object)
+
+
+// ==>another way
+
+// let key1 = "objkey1"
+// let key2 = "objkey2"
+
+// let value1 = "myValue1"
+// let value2 = "myValue2"
+
+// let object = {
+//     [key1] : value1,
+//     [key2] : value2
+// }
+
+// console.log(object)
