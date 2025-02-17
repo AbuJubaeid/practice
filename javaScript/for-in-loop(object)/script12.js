09275582649c347806fa7b48ade565b48fb60212
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-17 09:24:49 +0600

    Add files via upload

---
+++ javaScript/for-in-loop(object)/script12.js
@@ -0,0 +1,28 @@
+// **for in loop**
+
+// let person = {
+//     name : "rahim",
+//     age : 22,
+//     hobbies : ["playing", "reading", "writing"]
+// }
+// for(let man in person)
+// console.log(man, ":", person[man])
+
+
+// **Object.keys**
+
+// let person = {
+//     name : "rahim",
+//     age : 22,
+//     hobbies : ["playing", "reading", "writing"]
+// }
+// console.log(Object.keys(person))
+
+// let person = {
+//     name : "rahim",
+//     age : 22,
+//     hobbies : ["playing", "reading", "writing"]
+// }
+// for(i of Object.keys(person)){
+//     console.log(i, ":", person[i])
+// }
