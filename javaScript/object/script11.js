bf9bcab8e8259df8e2eddf14b14a58322b75c80f
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-17 09:23:04 +0600

    Add files via upload

---
+++ javaScript/object/script11.js
@@ -0,0 +1,21 @@
+// **object**
+
+// let person = {
+//     name : "rahim",
+//     age : 22,
+//     hobbies : ["playing", "reading", "writing"]
+// }
+// person.name = "karim"
+// person.hobbies[1] =  "biking"
+// person.hobbies.push("eating")
+// console.log(person)
+
+
+// let key = "email"
+// let person = {
+//     name : "rahim",
+//     age : 22,
+//     hobbies : ["playing", "reading", "writing"]
+// }
+// person[key] = "jubaeid@gmail.com"
+// console.log(person)
