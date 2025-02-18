4128128fef4a25878520fe38ac4dad5c250068a1
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-18 06:29:26 +0600

    Add files via upload

---
+++ javaScript/default-rest-parameter(function)/script15.js
@@ -0,0 +1,15 @@
+// **default parameter**
+
+// function myFunc(a, b=0){
+//     return a + b
+// }
+// console.log(myFunc(4, 5))
+
+
+// **rest parameter**
+// function myFunc(a, b, ...c){
+//     console.log("rest parameter", a)
+//     console.log("rest parameter", b)
+//     console.log("rest parameter",c)
+// }
+// myFunc(1, 2, 3, 4, 5, 6, 7)
