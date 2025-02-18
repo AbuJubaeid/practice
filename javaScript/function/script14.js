047231863db492391c2f6af54d9a12a949647faf
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-18 06:26:17 +0600

    Add files via upload

---
+++ javaScript/function/script14.js
@@ -0,0 +1,81 @@
+// **function**
+//  function declaration, function expression, arrow function
+
+
+// ==> function declaration
+
+// function myFunc(){
+//     console.log("this is a function")
+// }
+
+// myFunc()
+
+
+// ==>function expression
+
+// let myFunc = function (){
+//     console.log("this is a function")
+// }
+
+// myFunc() 
+
+// ==>arrow function
+
+// let myFunc = () => {
+//     console.log("this is a function")
+// }
+
+// myFunc() 
+
+
+
+// function myFunc(){
+//         return "this is a function too"
+//     }
+    
+//     console.log(myFunc())
+
+
+// let myFunc = function (){
+//     return "this is a function too"
+// }
+
+// console.log(myFunc())
+
+
+// let myFunc =  () => {
+//     return "this is a function too"
+// }
+
+// console.log(myFunc())
+
+
+
+// function myFunc(number){
+//     return number 
+// }
+
+// console.log(myFunc(4))
+
+
+// let myFunc = function myFunc(number){
+//     return number 
+// }
+
+// console.log(myFunc(4))
+
+
+// function sumThreeNum(num1, num2, num3){
+//     return num1 + num2 + num3
+// }
+// console.log(sumThreeNum(2, 3, 4))
+
+// let sumThreeNum = function (num1, num2, num3){
+//     return num1 + num2 + num3
+// }
+// console.log(sumThreeNum(2, 3, 4))
+
+// let sumThreeNum = (num1, num2, num3) => {
+//     return num1 + num2 + num3
+// }
+// console.log(sumThreeNum(2, 3, 4))
