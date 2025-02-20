d5d385c495efb0b7dd0085e47029650683d19723
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-20 09:05:14 +0600

    Add files via upload

---
+++ javaScript/16.parameter-destructuring(0bject)/script16.js
@@ -0,0 +1,12 @@
+// **parameter destructuring using function**
+
+// let person = {
+//     name : "karim",
+//     age : 22
+// } 
+
+// function myFunc(name, age){
+//     console.log(name)
+//     console.log(age)
+// }
+// myFunc(person)
