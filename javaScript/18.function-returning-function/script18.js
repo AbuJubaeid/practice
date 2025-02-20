a5d61c0d7b12aaee07b509c4287f8446f4b0ea4f
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-20 09:09:11 +0600

    Add files via upload

---
+++ javaScript/18.function-returning-function/script18.js
@@ -0,0 +1,10 @@
+// **function returning function**
+// function myFunc2(){
+//     console.log("main function called me")
+// }
+
+// function myFunc1(){
+//     return myFunc2
+// }
+// let turn = myFunc1()
+// turn()
