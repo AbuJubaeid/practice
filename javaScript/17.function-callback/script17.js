741486ac64f8053fbbbb2faa6136d280195a5853
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-02-20 09:06:57 +0600

    Add files via upload

---
+++ javaScript/17.function-callback/script17.js
@@ -0,0 +1,10 @@
+// **function callback**
+
+// function myFunc2(){
+//     console.log("main function called me")
+// }
+
+// function myFunc1(callback){
+//     callback()
+// }
+// myFunc1(myFunc2)
