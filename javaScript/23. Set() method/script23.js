3d64daa14db3e72cff615a48098086b211dcf13a
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-03-15 03:31:59 +0600

    Add files via upload

---
+++ javaScript/23. Set() method/script23.js
@@ -0,0 +1,15 @@
+// **set**
+
+let items = new Set()
+
+items.add(1)
+items.add(2)
+items.add(2)
+items.add(3)
+console.log(items)
+
+
+let ids = [1,2,3,4,4,5,6,6,7]
+
+let mySet = new Set(ids)
+console.log(mySet)
