baf0abdbc8f53657ab530e9f35b6a690194220bf
AbuJubaeid <abujubaeidpranto2@gmail.com>
2025-03-17 23:36:28 +0600

    Update script26.js

--- javaScript/26. call() method/script26.js
+++ javaScript/26. call() method/script26.js
@@ -1,1 +1,39 @@
+// **call() method**
+
+let user1 = {
+    firstName : "Jubaeid",
+    age : 22,
+    about : function(){
+        console.log(this.firstName, this.age)
+    }
+}
+
+let user2 = {
+    firstName : "Muntaha",
+    age : 15
+}
+
+user1.about()
+user1.about.call(user2)
+
+
+// ===> another way
+
+function about(hobby, favourite){
+    console.log(this.firstName, this.age, hobby, favourite)
+}
+
+
+let user3 = {
+    firstName : "Jubaeid",
+    age : 22,
+    
+}
+
+let user4 = {
+    firstName : "Muntaha",
+    age : 15
+}
+
+about.call(user1, "gardening", "riding")
 
